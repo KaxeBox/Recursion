466bea626646c0cd5a937dd035e51e477c540d91
kulsumbegum <kulsumbegum122@gmail.com>
2022-02-02 19:25:56 +0530

    Modified

--- Powerlogarithmic.java
+++ Powerlogarithmic.java
@@ -6,12 +6,13 @@ public class Powerlogarithmic {
         int x=sc.nextInt();
         System.out.println(power(x,n));
     }
-    public static int power(int x, int n){
+    public static double power(int x, int n){
         if(n==0) return 1;
         // else if(n%2==0)return power(x,n/2)*power(x,n/2);
         // else return (x*power(x,n/2)*power(x,n/2));
-        int x1=power(x,n/2);
-        int xnbp=x1*x1;
+        if(n<0) return 1/power(x,-n);
+        double x1=power(x,n/2);
+        double xnbp=x1*x1;
         if(n%2!=0)xnbp=xnbp*x;
         return xnbp;
     }
